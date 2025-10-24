37d4d2c26dcc331524d32c6127183b66bac09631
upotsu <apeiron365@outlook.jp>
2025-10-25 07:31:05 +0900

    Add files via upload

---
+++ MovingPlatform.h
@@ -0,0 +1,36 @@
+	// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class OBSTACLEASSULT_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	//UEにメンバーメモリフロートを登録しエディタ内で表示する
+	float MemberFloat = 10.0f;
+
+	UPROPERTY(EditAnywhere)
+	int MemberInt = 5;
+
+	UPROPERTY(EditAnywhere)
+	FVector MyVector = FVector(11.2f, 3.4f, 6.5f);
+	
+};
